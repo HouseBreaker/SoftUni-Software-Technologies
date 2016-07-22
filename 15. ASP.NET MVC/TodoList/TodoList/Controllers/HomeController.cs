154f36cbf57cb560857b3149dbcf48a9db7b9b30
Housey <housebreaker1@gmail.com>
2016-07-22 18:48:02 +0300

    mvc exercises

---
+++ 15. ASP.NET MVC/TodoList/TodoList/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TodoList.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var items = (List<string>) this.Session["items"] ?? new List<string>();
+
+			return View(items);
+		}
+
+		[HttpPost]
+		[ValidateInput(false)]
+		public ActionResult AddItem(string newItem)
+		{
+			var items = GetTodoItemsFromStorage();
+
+			items.Add(newItem);
+			this.Session["items"] = items;
+
+			return this.RedirectToAction("Index");
+		}
+
+		public ActionResult RemoveItem(int index)
+		{
+			var items = GetTodoItemsFromStorage();
+
+			if (items != null && index < items.Count)
+			{
+				items.RemoveAt(index);
+				this.Session["items"] = items;
+			}
+
+			return this.RedirectToAction("Index");
+		}
+
+		private List<string> GetTodoItemsFromStorage()
+		{
+			var items = (List<string>)this.Session["items"] ?? new List<string>();
+			return items;
+		}
+	}
+}
